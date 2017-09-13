88cfe447bd1c0bf77a77f044e848f55ada77af54
float.sundae@gmail.com <float.sundae@gmail.com>
2017-09-13 15:05:05 +0800

    ito sabaw ni Cortez

---
+++ sabaw/index.php
@@ -0,0 +1,605 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+
+
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light blue-grey lighten-5">
+
+    <!-- Navbar brand -->
+  
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link page-scroll" href="#aboutflowtork">About Flowtork</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+
+            <!-- Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+
+        </ul>
+        <!-- Links -->
+
+        <!-- Search form -->
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        </form>
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+<body>
+<!--Carousel Wrapper-->
+<div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+    <!--Indicators-->
+    <ol class="carousel-indicators">
+        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+    </ol>
+    <!--/.Indicators-->
+    <!--Slides-->
+    <div class="carousel-inner" role="listbox">
+        <!--First slide-->
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg" alt="First slide">
+        </div>
+        <!--/First slide-->
+        <!--Second slide-->
+        <div class="carousel-item">
+            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg" alt="Second slide">
+        </div>
+        <!--/Second slide-->
+        <!--Third slide-->
+        <div class="carousel-item">
+            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" alt="Third slide">
+        </div>
+        <!--/Third slide-->
+    </div>
+    <!--/.Slides-->
+    <!--Controls-->
+    <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+    <!--/.Controls-->
+</div>
+<!--/.Carousel Wrapper-->
+
+
+
+
+<br><br>
+
+    <!-- Start your project here-->
+<section id="aboutflowtork">
+    
+
+
+<div class="container">
+<div class="col-md-12">
+<h2>HISTORY</h2><br>
+<p>Flowtork Technologies Corporation, formerly known as Flowtork Industrial Corporation, was established on September 23, 2004 by a group of young professionals with technical know-how who were by then been well-trained and experienced to provide comprehensive and prompt technical services on all the products they offer. The business , after a corporate re-structuring, was transformed as Flowtork Technologies Corporation on December 10, 2007.</p>
+
+<p>Flowtork Technologies Corporation has been engaged in supply of industrial instrumentation equipment such as electric and pneumatic actuators, valves, flow meters, mixers, agitators and marine loading arms to vital industries in the Philippines. In addition , the company also provides technically-skilled manpower and after-sales services of the products supplied to its various clients.</p>
+
+<p>The company has continued to expand as more professional and technical people have been added to its roster in order to cater to its growing list of clientele. In September 2012, Flowtork Technologies Corporation opened its Cebu City branch. With the growing demand for instrumentation equipment and services, Flowtork envisions itself to remain in the forefront of servicing this demand though continuous improvements in its products and services. With its history and track record, clients are assured that Flowtork will continue with its commitment of service which adds real value to the client's business.</p>
+</div>
+</div>
+<div class="container">
+<div class="col-md-12">
+<h2>INDUSTRIES SERVED</h2><br>
+<div class="row">
+<div class="col-md-4" style="font-size: 15px;">
+• Power<br> 
+• Cement<br>
+• Manufacturing Plant<br>
+• Oil & Gas<br>
+• Electronics / Semiconductor<br>
+• Steel Mills<br>
+• Mining & Ore Processing<br>
+</div>
+<div class="col-md-4" style="font-size: 15px;">   
+• Food & Beverages<br>
+• Pharmaceuticals<br>
+• Water & Waste Water<br>
+• Petrochemicals<br>
+• Pulp & Paper<br>
+• Bio-energy (Power)<br>
+• Sugar<br>
+</div>
+</div>
+</div>
+</div>
+
+<br>    
+<div class="container">
+<div class="col-md-12">
+<h2>FIELD OF EXPERTISE</h2><br>
+<div class="row">
+<div class="col-md-4">
+<b>Field / Shop Servicing</b><br> 
+• Diagnostic<br>
+• Installation & Retrofitting<br>
+• Product & System Upgrades<br>
+• After-Sales Service<br>
+• Maintenance Contracts<br>
+• Product Training & Hands-on<br>
+
+
+</div>
+<div class="col-md-4">
+<b>Instrumentation & Electrical Works</b><br> 
+• Installation & Commissioning<br>
+• Engineering & Maintenance<br>
+• Verification & Calibration Assistance<br>
+• Design, Sizing & Selection for new application<br>
+
+
+</div>
+<div class="col-md-4">
+<b>System Integration</b><br>  
+• Foundation Fieldbus<br>
+• Profibus<br>
+• Devicenet<br>
+• Modbus Integration<br>
+• PLC / SCADA System<br>
+
+
+</div>
+
+
+
+
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="container">
+
+</div>
+<br><br><br>
+
+
+<div class="container">
+
+<div class="row">
+
+<!--Card-->
+<div class="card col-md-4">
+
+    <!--Card image-->
+    <div class="view overlay hm-white-slight"><center><br>
+ <i class="fa fa-eye" style="font-size: 102px;"></i></center>
+    </div>
+
+    <!--Card content-->
+    <div class="card-body">
+        <!--Title-->
+        <h4 class="card-title"><center>Our Vision</center></h4>
+        <!--Text-->
+        <p class="card-text">WE WILL ALWAYS ENDEAVOR to be one of the leaders in process instrumentation products in terms of continuous growth, profitability and judiciously utilizing our resources in pursuit of quality and innovation after-sales service.</p>
+
+        <p class="card-text">Verily, to actively enjoined our peers in nation-building and ultimately contributing quality life for our workers and every Filipino in general.</p>
+    </div>
+
+</div>
+<!--/.Card-->
+                
+
+
+
+
+
+<!--Card-->
+<div class="card col-md-4">
+
+    <!--Card image-->
+    <div class="view overlay hm-white-slight">
+
+<center><br>
+ <i class="fa fa-industry" style="font-size: 102px;"></i></center>
+
+    </div>
+
+    <!--Card content-->
+    <div class="card-body">
+        <!--Title-->
+        <h4 class="card-title"><center>Mission</center></h4>
+        <!--Text-->
+
+
+        <p class="card-text">We shall build a well-trained, competent and customer oriented instrumentation company, safely following established standards without compromise and maintain a working force in an atmosphere that provides competitive financial package thus promoting opportunities for professional excellence.</p>
+
+
+        <p class="card-text">In light of the above, we shall be guided by our core values of honesty and integrity, customer satisfaction and of course teamworks for our people.</p>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+<!--Card-->
+<div class="card col-md-4">
+
+    <!--Card image-->
+    <div class="view overlay hm-white-slight">
+
+<center><br>
+ <i class="fa fa-black-tie" style="font-size: 102px;"></i></center>
+
+    </div>
+
+    <!--Card content-->
+    <div class="card-body">
+        <!--Title-->
+        <h4 class="card-title"><center>Corporate Principle</center></h4>
+        <!--Text-->
+        <p class="card-text">To maintain market visibility in the instrumentation industry.</p>
+
+
+        <p class="card-text">To achieve customer delight through prompt and quality service, responsive and innovative products and services, efficient organizational structure and administrative capabilities.</p>
+
+        <p class="card-text">To establish competitive-edge through improved process instrumentation facilities and use of well-trained force of professionals.</p>
+
+    </div>
+
+</div>
+
+
+
+</div>
+<!--/.Card-->
+
+
+
+
+</div>
+</section>
+
+
+<br><br><br>
+
+
+
+<section id="tabs">
+    
+<div class="container">
+<div class="col-md-12">
+
+                        
+<!-- Nav tabs -->
+<ul class="nav nav-tabs nav-justified">
+    <li class="nav-item">
+        <a class="nav-link active" data-toggle="tab" href="#panel1" role="tab">Major Suppliers</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel2" role="tab">Major Clients</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel3" role="tab">Services</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" data-toggle="tab" href="#panel4" role="tab">Sales & info Desk</a>
+    </li>
+</ul>
+<!-- Tab panels -->
+<div class="tab-content card">
+    <!--Panel 1-->
+    <div class="tab-pane fade in show active" id="panel1" role="tabpanel">
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+            reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+            porro voluptate odit minima.</p>
+    </div>
+    <!--/.Panel 1-->
+    <!--Panel 2-->
+    <div class="tab-pane fade" id="panel2" role="tabpanel">
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+            reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+            porro voluptate odit minima.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+            reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+            porro voluptate odit minima.</p>
+    </div>
+    <!--/.Panel 2-->
+    <!--Panel 3-->
+    <div class="tab-pane fade" id="panel3" role="tabpanel">
+        <br>
+<div class="container">
+        • 24/7 after sales support<br>
+        • Preventive Maintenance Inspection (PMI)<br>
+       • Installation, Testing and Commissioning<br>
+       • Overhauling & Repair<br>
+       • System Integration<br>
+       • Product Training & Seminar<br>
+       • Installation of electric actuators, valves & flowmeters at large pipe sizes<br>
+       • Customizations for Special Application<br>
+       • Consultation & Engineering<br>
+       • Flow Measurement Data Logging <br>
+
+
+</div>
+
+
+
+    </div>
+    <!--/.Panel 4-->
+    <div class="tab-pane fade" id="panel4" role="tabpanel">
+        <br>
+<div class="container">
+<div class="row">
+      <img class="img-fluid" src="img/ftclogo.png"> <h2 class="section-heading h1 pt-4">&nbsp;Flowtork Technologies Corp.</h2></div><br>
+<!--Section: Contact v.2-->
+<section class="section">   
+
+    <!--Section heading-->
+    <h4>Inquiry Form</h4>
+    <!--Section description-->
+    <p class="section-description">Information: Please send your inquiry to our corporate mailbox thru simple form below.
+Thank you.</p>
+
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-8 col-xl-9">
+            <form id ="contact-form" name="contact-form" action="mail.php" method="POST">
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form">
+                            <div class="md-form">
+                                <input type="text" id="name" name="name" class="form-control">
+                                <label for="name" class="">Your name</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form">
+                            <div class="md-form">
+                                <input type="text" id="email" name="email" class="form-control">
+                                <label for="email" class="">Your email</label>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Subject</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" class="md-textarea"></textarea>
+                            <label for="message">Your message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="center-on-small-only">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            </div>
+            <div class="status" id="status"></div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-4 col-xl-3">
+
+            <ul class="contact-icons">
+                <li><i class="fa fa-map-marker fa-2x"></i>
+Contact Address: <br>
+Unit 1, 2 & 10 CIFRA Building<br>
+#641 Boni Avenue, Mandaluyong City<br>
+1550 PHL<br>
+                </li>
+
+                <li><i class="fa fa-phone fa-2x"></i>
+                  Telephone:<br> + (63-2) 470 43 58<br>
+Fax: + (63-2) 534 12 17
+                </li>
+
+            </ul>
+        </div>
+        <!--Grid column-->
+
+    </div>
+
+</section>
+<!--Section: Contact v.2-->
+
+</div>
+    </div>
+    <!--/.Panel 4-->
+</div>
+            
+
+
+
+
+
+</div>
+
+</div>
+</section>
+
+
+<br><br><br>
+
+<div class="container">
+<div class="col-md-12">
+<h2>Contact Us</h2>
+<h3>Flowtork Technologies Corporation </h3><br>
+Email: info@flowtork.com<br>
+http://www.flowtork.com
+<br><br>
+
+
+
+</div>
+<div class="col-md-12">
+<div class="row">
+<div class="col-md-6">
+<br><br>    
+
+<h4>HEAD OFFICE</h4>
+<br>
+Unit 1,2 & 10, 2nd Floor, CIFRA Building<br>
+#641 Boni Avenue<br>
+Mandaluyong City 1550 Philippines<br>
+<br>    
+Contact No(s):<br>
+Sales Department : (Direct Lines)      &nbsp; &nbsp;  +(63-2) 470 43 58 <br>
++(63-2) 534 12 17<br>
+Accounting & HR Department :        &nbsp; &nbsp; &nbsp; &nbsp; +(63-2) 532 40 58<br>
+Projects and Services Department :      +(63-2) 748 97 36<br>
+</div>
+
+
+<div class="col-md-6">
+<br><br>    
+
+<h4>CEBU OFFICE</h4>
+<br>
+4th Floor Ongtiak Building<br>
+General Maxilom Avenue
+<br>
+Cebu City 6000 Philippines<br>
+<br>    
+Contact No(s):<br>
+Sales and Services Department :
+    +(63-32) 406 33 50 <br>
+ &nbsp; &nbsp; &nbsp; &nbsp;+(63-32) 250 65 82<br>
+ &nbsp; &nbsp; &nbsp; &nbsp;+(63-917) 863 23 66<br>
+  &nbsp; &nbsp; &nbsp; &nbsp;+(63-939) 920 91 08<br>
+</div>
+
+
+
+</div>
+</div>
+</div>
+
+
+
+
+
+
+    <!-- /Start your project here-->
+<br><br><br><br>
+<!--Footer-->
+<footer class="page-footer stylish-color-dark">
+
+    <!--Footer Links-->
+
+    <!--/.Footer Links-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2015 • Flowtork Technologies Corporation</a>
+
+        </div>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+<!--/.Footer-->
+                
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+
+</html>
