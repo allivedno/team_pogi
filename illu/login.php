c373c46e066d35981c85e7134b72a25c41061194
cjayconocono <lawrence.r.deleon@gmail.com>
2017-09-14 14:54:29 +0800

    admin login

--- illu/login.php
+++ illu/login.php
@@ -13,6 +13,8 @@
         if (!empty($user)) {
 
             echo "login seccessful";
+            $_SESSION[WEBAPP]['user'] = $user['id'];
+            redirect('index.php');
         }else {
             echo encryptIt($_POST['password']);
             echo "wrong username or password";
@@ -24,54 +26,3 @@
 
 ?>
 
-    
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <title>Login to flowtork</title>
-    <link href="css/login.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-    <script>
-        $(function(){
-          var $form_inputs =   $('form input');
-          var $rainbow_and_border = $('.rain, .border');
-          /* Used to provide loping animations in fallback mode */
-          $form_inputs.bind('focus', function(){
-            $rainbow_and_border.addClass('end').removeClass('unfocus start');
-          });
-          $form_inputs.bind('blur', function(){
-            $rainbow_and_border.addClass('unfocus start').removeClass('end');
-          });
-          $form_inputs.first().delay(800).queue(function() {
-            $(this).focus();
-          });
-        });
-    </script>
-        
-    </head>
-    <body id="home">
-        <div class="rain">
-            <div class="border start">
-                <form>
-                    <br><br>
-                    <label for="email">Email</label><br>
-                    <input name="username" type="text" placeholder="Username"/>
-                    <label for="pass">Password</label>
-                    <input name="password" type="password" placeholder="Password"/>
-                    <input type="submit" value="LOG IN"/>
-                    <br><br><br>
-                    <h5><center><a href='forgot.php' STYLE="text-decoration: none"><font color = 'red'>Forgot Password?</font></a></h5>
-                <h4><center><font color = 'white'>Don't have an account yet? </font><a href='signup.php' STYLE="text-decoration: none"><font color = 'red'>Register now</h4>
-                </form>
-                <br><br>
-                
-
-            </div>
-        </div>
-    </body>
-</html>
-
-</body>
-</html>
