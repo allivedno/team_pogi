918c42c68b161f6163465f71924a37e6b494d9ef
wadewilson <miguel_ccortez@yahoo.com.ph>
2017-09-30 21:09:02 +0800

    wew

--- sabaw/config.connect.php
+++ sabaw/config.connect.php
@@ -1,7 +1,7 @@
 <?php 
 $h1='localhost';
 $u1='root';
-$p1='ondevilla';
+$p1='miguel';
 $d1="flowtork";
 $c1=mysqli_connect($h1, $u1, $p1, $d1) or die (mysqli_error());
 ?>
--- sabaw/header.php
+++ sabaw/header.php
@@ -7,7 +7,7 @@
 <html lang="en">
 <head>
  <title>Flowtork</title>
-<link rel="icon" type="image/png" sizes="32x32" href="img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="32x32" href="img/minicon.png">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -21,85 +21,63 @@
     <!-- Your custom styles (optional) -->
     <link href="css/style.css" rel="stylesheet">
 
-
-
+    <link href="css/arnold.css" rel="stylesheet">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
 
 <link href="css/login.css" rel="stylesheet">
 
 </head>
+<div class="bgded" style="background-image:url('img/banner2.png'); height: 220px; "> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1" >
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+     
+      </div>
+      <nav id="mainav" class="fl_right" >
+        <ul class="clear">
+          <li><a href="index.php">Home</a></li>
+    
+          <li><a href="index.php#aboutflowtork">About Flowtork</a></li>
+          <li><a href="products.php">Products</a></li>
+          <li><a href="projects.php">Projects</a></li>
+          <li><a href="index.php#contactus">Contact Us</a></li>
 
 
-<body>
-
-
-
-<nav class="navbar navbar-expand-lg navbar-light blue-grey lighten-5">
-
-    <!-- Navbar brand -->
-  
-
-    <!-- Collapse button -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-
-    <!-- Collapsible content -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-        <!-- Links -->
-        <ul class="navbar-nav mr-auto ">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link page-scroll" href="index.php#aboutflowtork">About Flowtork</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="products.php">Products</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="projects.php ">Projects</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link page-scroll" href="index.php#contactus">Contact Us</a>
-            </li>
-            <li>
-         
-            </li>
-            </li>
-            <!-- Dropdown -->
             <?php if(!isLoggedIn()): ?>
                 <li class="nav-item dropdown">
-                    <a class="nav-link page-scroll" href="frmlogin.php">Login</a>      
+                    <a href="frmlogin.php">Login</a>      
                 </li>
             <?php else: ?>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?php echo $_SESSION[WEBAPP]['user']; ?></a>
-                    </button>
 
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="logout.php">Log-out</a>
-                    </div>
-               
-                   
-                </li>
-            <?php endif; ?>
 
+
+          <li><a class="drop" href="#"><?php echo $_SESSION[WEBAPP]['user']; ?>Dropdown</a>
+            <ul>
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li>     <a class="dropdown-item" href="#">Another action</a></li>
+
+
+
+              <li><a class="dropdown-item" href="logout.php">Log-out</a></li>
+            </ul>
+          </li>
+              <?php endif; ?>
+
+ 
 
         </ul>
-        <!-- Links -->
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
 
-        <!-- Search form 
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-        </form>-->
-    </div>
-    <!-- Collapsible content -->
+  <!-- ################################################################################################ -->
+</div>
 
-</nav>
 
-</body>
 </html>
